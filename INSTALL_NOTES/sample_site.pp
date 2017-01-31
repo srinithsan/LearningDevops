10c27befce1853167187429a5747f998c39a004a
Seshagiri Sriram <SehsagiriSriram@gmail.com>
2017-01-31 17:00:43 +0530

    Added Misc Scripts - TIME TO CLEAN UP

---
+++ INSTALL_NOTES/sample_site.pp
@@ -0,0 +1,4 @@
+hiera_include('classes') 
+
+Class A inherits BClass B inherits C
+Class C inerits D
--- SRC_CODE_EDUREKA_DEMO_PUPPET/Solution_2/java_tomcat_code.pp
+++ SRC_CODE_EDUREKA_DEMO_PUPPET/Solution_2/java_tomcat_code.pp
@@ -1,5 +1,6 @@
 #install java 
 class tomcat { 
+	require java 
 	group{'tomcat':
 		name =>'tomcat',
 		ensure => present,
@@ -27,6 +28,7 @@ class tomcat {
 		require => [ Exec['extracttomcat'], User['tomcat'] ] ,
 		command => 'chgrp -R tomcat /opt/tomcat && /opt/tomcat/bin/startup.sh',
 		cwd =>'/opt/tomcat/bin',
+		source => puppet:///tomcat/xyz.txt
 	}
 }
 
@@ -42,8 +44,23 @@ class java {
 		path => '/bin:/usr/sbin:/usr/bin:/sbin',
 		command => 'add-apt-repository -y ppa:openjdk-r/ppa && apt-get update && apt-get install -y openjdk-8-jdk',
 		user => 'root',
-	} 
+	}
 }
 
-class {'java':}->class{'tomcat':}
 
+class {'java':'distribution' => 'jre', 'version'  => '7.0'}-> class{'tomcat':} #Before -> Notify ~>
+#class {'tomcat':} 
+
+#Java 7 -> Tomcat 7 (tomcat 7 service is runinng) 
+#Java 8 -> Tomcat 7 (Java 8 will be installed, will not restart the tomcat 7 service0 
+#Java 8 ~> Tomcat 7 (Java 8 will be installed and tomcat 7 service will be restarted) 
+#Java 8 ~> Tomcat 8 (Will skip java8, install Tomcat 8 and start tomcay 8 service) 
+
+
+$_  = 10
+$__ = 20 
+
+$a = 10 
+$b = 20 
+$c = 30 
+[$a, $b, $c] = [10,20,30] 
